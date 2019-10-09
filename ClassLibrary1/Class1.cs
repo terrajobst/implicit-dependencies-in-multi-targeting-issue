2172bd1b35c29b4845b23fe6dc56722fd6ab5f4e
Immo Landwerth <immol@microsoft.com>
2019-10-09 15:41:34 -0700

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1 : IAsyncDisposable
+    {
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine(class1);
+        }
+    }
+}
